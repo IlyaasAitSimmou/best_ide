9a1f7ac4a2e012d616d48ba8f928a0c4eeafbd48
YufusKhan <67281394+YufusKhan@users.noreply.github.com>
2024-08-13 15:19:19 -0700

    Worked on accounts

--- backend/IDE/mongoDB.py
+++ backend/IDE/mongoDB.py
@@ -1,13 +1,31 @@
 import os
 import pymongo
+import uuid
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 
-cluster=MongoClient(os.getenv('ATLAS_URI'))
-
+cluster=MongoClient(os.getenv("ATLAS_URI"))
 db = cluster['best_ide']
-collection = db['projects']
+projects = db['projects']
+accounts = db['accounts']
+print("Connected to MongoDB")
+# account object to save in db: id, email, username, password, verified, logged_in
 
 
 
-# Code for saving/updating/deleting files
+def addAccount(email, username, password):
+    account = {
+        "_id": str(uuid.uuid4()),
+        "email": email,
+        "username": username,
+        "password": password,
+        "verified": False,
+        "logged_in": False
+    }
+    accounts.insert_one(account)
+    return account
+
+
+def sendVerificationEmail(email):
